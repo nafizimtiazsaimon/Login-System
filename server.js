d7a47c689a779dd46925b8665c08418bcf7497c8
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-07-23 20:48:08 +0600

    login form front

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+
+//load static assests
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) =>{
     res.render('base', { title: "Login System"});
