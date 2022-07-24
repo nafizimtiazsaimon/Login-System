7009d6ac7b5c7fe619be2ead6b2b664dc75d620e
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-07-24 22:24:10 +0600

    log out

--- router.js
+++ router.js
@@ -11,11 +11,30 @@ const credential = {
 router.post('/login', (req, res) => {
     if(req.body.email == credential.email && req.body.password == credential.password){
         req.session.user = req.body.email;
-        res.end("Login Successful");
-        //res.redirect('/dashboard');
+        //res.end("Login Successful");
+        res.redirect('/route/dashboard');
     }else{
         res.end("Invalid email or password")
     }
 });
+
+router.get('/dashboard', (req, res) => {
+    if(req.session.user){
+        res.render('dashboard', {user: req.session.user})
+    }else{
+        res.send("Unauthorized")
+    }
+})
+
+router.get('/logout', (req, res) => {
+    req.session.destroy(function(err){
+        if(err){
+            console.log(err);
+            res.send("Error")
+        }else{
+            res.render('base', { title: "Login", logout: "Logout Successful"})
+        }
+    })
+})
 
 module.exports = router;
